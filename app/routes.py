e379db0703ec9536bdc0c7f770c1aa726d760655
zqf0722 <zqf0722@mail.ustc.edu.cn>
2021-11-02 19:45:13 -0400

    Tempaltes and Web Forms added

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
+def home():
+    return render_template('base.html', title='Welcome')
+
 @app.route('/index')
 def index():
     user = {'username':'Qifeng'}
@@ -16,3 +20,13 @@ def index():
         }
     ]
     return render_template('index.html',title = 'Home', user = user, posts = posts)
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title = 'Sign In', form = form)
