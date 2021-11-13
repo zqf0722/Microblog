0a012771d9b8fe2baaf39e62e716cd1eed551a56
zqf0722 <zqf0722@mail.ustc.edu.cn>
2021-11-13 00:59:15 -0500

    elasticsearch create problems

--- app/search.py
+++ app/search.py
@@ -1,11 +1,23 @@
 from flask import current_app
 
+def es_create_index_if_not_exists(es, index):
+    """Create the given ElasticSearch index and ignore error if it already exists"""
+    try:
+        es.indices.create(index)
+    except es.exceptions.RequestError as ex:
+        if ex.error == 'resource_already_exists_exception':
+            pass # Index already exists. Ignore.
+        else: # Other exception - raise it
+            raise ex.error
+
+
 def add_to_index(index, model):
     if not current_app.elasticsearch:
         return
     payload = {}
     for field in model.__searchable__:
         payload[field] = getattr(model, field)
+    es_create_index_if_not_exists(current_app.elasticsearch, index)
     current_app.elasticsearch.index(index=index, id=model.id, body=payload)
 
 def remove_from_index(index, model):
