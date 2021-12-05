add94ab080b456cb19631c9be86224e939aa2163
zqf0722 <zqf0722@mail.ustc.edu.cn>
2021-12-05 18:16:32 -0500

    change what unauthenticated user can see

--- app/main/routes.py
+++ app/main/routes.py
@@ -22,27 +22,30 @@ def before_request():
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
-@login_required
 def index():
-    Post.reindex()
-    form = PostForm()
-    if form.validate_on_submit():
-        try:
-            language = detect(form.post.data)
-        except LangDetectException:
-            language = ''
-        post = Post(body=form.post.data, author=current_user, language=language)
-        db.session.add(post)
-        db.session.commit()
-        flash(_('Your post is now live!'))
-        return redirect(url_for('main.index'))
-    page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().paginate(
-        page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
-    return render_template('index.html', title='Home', form=form,
-                           posts=posts.items, next_url=next_url, prev_url=prev_url)
+    if current_user.is_authenticated:
+        Post.reindex()
+        form = PostForm()
+        if form.validate_on_submit():
+            try:
+                language = detect(form.post.data)
+            except LangDetectException:
+                language = ''
+            post = Post(body=form.post.data, author=current_user, language=language)
+            db.session.add(post)
+            db.session.commit()
+            flash(_('Your post is now live!'))
+            return redirect(url_for('main.index'))
+        page = request.args.get('page', 1, type=int)
+        posts = current_user.followed_posts().paginate(
+            page, current_app.config['POSTS_PER_PAGE'], False)
+        next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
+        prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
+        return render_template('index.html', title='Home', form=form,
+                               posts=posts.items, next_url=next_url, prev_url=prev_url)
+    else:
+        return redirect(url_for('main.explore'))
+
 
 
 @bp.route('/follow/<username>', methods=['POST'])
@@ -98,7 +101,7 @@ def user(username):
         if posts.has_prev else None
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts.items,
-                           form = form, next_url=next_url, prev_url=prev_url)
+                           form=form, next_url=next_url, prev_url=prev_url)
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
@@ -119,7 +122,6 @@ def edit_profile():
 
 
 @bp.route('/explore')
-@login_required
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
@@ -128,12 +130,15 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template("index.html", title='Explore', posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
+    if current_user.is_authenticated:
+        return render_template("index.html", title='Explore', posts=posts.items,
+                               next_url=next_url, prev_url=prev_url)
+    else:
+        return render_template("index.html", title='Explore', posts=posts.items,
+                               next_url=next_url, prev_url=prev_url, notlogin=True)
 
 
 @bp.route('/translate', methods=['POST'])
-@login_required
 def translate_text():
     return jsonify({'text': translate(request.form['text'],
                                       request.form['source_language'],
@@ -141,7 +146,6 @@ def translate_text():
 
 
 @bp.route('/search')
-@login_required
 def search():
     if not g.search_form.validate():
         return redirect(url_for('main.explore'))
@@ -157,7 +161,6 @@ def search():
 
 
 @bp.route('/user/<username>/popup')
-@login_required
 def user_popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     form = EmptyForm()
